1217978b42bd916256f54caafe3be96093301706
Euticus Kamau <euticus.dev@gmail.com>
2026-08-31 13:23:47 +0300

    Create OpenAI client wrapper

--- src/openAI.php
+++ src/openAI.php
@@ -2,17 +2,30 @@
 
 namespace Euticus\WhatsappAi;
 
+use OpenAI\Client as OpenAIClient;
+use \OpenAI as OpenAISDK;
+
 class OpenAI
 {
     private string $apiKey;
+    private ?OpenAIClient $client = null;
 
     public function __construct()
     {
         $this->apiKey = $_ENV['OPENAI_API_KEY'] ?? '';
+
+        if ($this->apiKey !== '') {
+            $this->client = OpenAISDK::client($this->apiKey);
+        }
     }
 
     public function hasApiKey(): bool
     {
         return $this->apiKey !== '';
+    }
+
+    public function getClient(): ?OpenAIClient
+    {
+        return $this->client;
     }
 }
